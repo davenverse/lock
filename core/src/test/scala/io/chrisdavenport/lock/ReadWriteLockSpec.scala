177934a9fe5297c533175661f6e861c77958aab6
Christopher Davenport <chris@christopherdavenport.tech>
2021-09-16 20:26:28 -0700

    Additional ReadWriteLock tests

--- core/src/test/scala/io/chrisdavenport/lock/ReadWriteLockSpec.scala
+++ core/src/test/scala/io/chrisdavenport/lock/ReadWriteLockSpec.scala
@@ -58,4 +58,59 @@ class ReadWriteLockSpec extends CatsEffectSuite {
     } yield assertEquals(true, true, "Good if we don't lock above")
   }
 
+  test("ReadWriteLock Write Downgrade lock is still held"){
+    for {
+      lK <- ReadWriteLock.reentrant[IO]
+      l1 <- ReadWriteLock.rentrantUnique(lK)
+      l2 <- ReadWriteLock.rentrantUnique(lK)
+      _ <- l1.writeLock.lock
+      out1 <- l2.writeLock.tryLock
+      _ <- l1.readLock.lock 
+      _ <- l1.writeLock.unlock
+      out2 <- l2.writeLock.tryLock
+    } yield assertEquals((out1, out2), (false, false))
+  }
+
+  test("ReadWriteLock Write Downgrade lock is still held, but allows readers"){
+    for {
+      lK <- ReadWriteLock.reentrant[IO]
+      l1 <- ReadWriteLock.rentrantUnique(lK)
+      l2 <- ReadWriteLock.rentrantUnique(lK)
+      _ <- l1.writeLock.lock
+      out1 <- l2.readLock.tryLock
+      _ <- l1.readLock.lock 
+      _ <- l1.writeLock.unlock
+      out2 <- l2.readLock.tryLock
+    } yield assertEquals((out1, out2), (false, true))
+  }
+
+  test("ReadWriteLock Read Lock allows additional readers"){
+    for {
+      lK <- ReadWriteLock.reentrant[IO]
+      l1 <- ReadWriteLock.rentrantUnique(lK)
+      l2 <- ReadWriteLock.rentrantUnique(lK)
+      _ <- l1.readLock.lock
+      out <- l2.readLock.tryLock
+    } yield assertEquals(out, true)
+  }
+
+  test("ReadWriteLock Read Lock is reentrant"){
+    for {
+      lK <- ReadWriteLock.reentrant[IO]
+      l1 <- ReadWriteLock.rentrantUnique(lK)
+      _ <- l1.readLock.lock
+      out <- l1.readLock.tryLock
+    } yield assertEquals(out, true)
+  }
+
+  test("ReadWriteLock ReadLock will wake write wait"){
+    for {
+      lK <- ReadWriteLock.reentrant[IO]
+      l1 <- ReadWriteLock.rentrantUnique(lK)
+      l2 <- ReadWriteLock.rentrantUnique(lK)
+      _ <- l1.readLock.lock
+      _ <- (l2.writeLock.lock, Temporal[IO].sleep(100.millis) >> l1.readLock.unlock).parTupled
+    } yield assertEquals(true, true, "If we get here we're not block")
+  }
+
 }
